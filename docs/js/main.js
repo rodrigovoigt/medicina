d80afbc84a8d2b5d6179611f320294180fb84c8a
Rodrigo Voigt Filho <rodrigo.voigt.f@gmail.com>
2025-02-06 16:20:23 -0300

    dsadsad

--- docs/js/main.js
+++ docs/js/main.js
@@ -33,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const links = {
+        homeLink: "home",
         riskLink: "riskCalculator",
         ldlLink: "ldlCalculator",
         gfrLink: "gfrCalculator",
