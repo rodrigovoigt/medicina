383153379c5c290c668e6cfd6e5fccd37f9a12b4
Rodrigo Voigt Filho <rodrigo.voigt.f@gmail.com>
2025-02-06 21:49:10 -0300

    risco final

--- docs/js/main.js
+++ docs/js/main.js
@@ -103,62 +103,148 @@ $(document).ready(function () {
         copiarTexto($(this).attr('id'));
     });
 
-    // Calculadora de Colesterol LDL
+    // Calculadora de Risco Cardiovascular em 10 anos
     $('#calcularRiscoBtn').click(function () {
         let idade = parseFloat($('#idade').val());
         let colesterolTotal = parseFloat($('#colesterolTotal').val());
         let colesterolHDL = parseFloat($('#colesterolHDL').val());
         let pressaoSistolica = parseFloat($('#pressaoSistolica').val());
-        let emMedicamentoHipertensao = $('#emMedicamentoHipertensao').val() === "true" ? 1 : 0;
-        let fumante = $('#fumante').val() === "true" ? 1 : 0;
-        let diabetes = $('#diabetes').val() === "true" ? 1 : 0;
+        let emMedicamentoHipertensao = $('#emMedicamentoHipertensao').val() === 'true';
+        let fumante = $('#fumante').val() === 'true';
+        let diabetes = $('#diabetes').val() === 'true';
         let genero = $('#genero').val();
         let raca = $('#raca').val();
-
-        if (isNaN(idade) || isNaN(colesterolTotal) || isNaN(colesterolHDL) || isNaN(pressaoSistolica)) {
-            alert('Preencha todos os campos corretamente!');
-            return;
-        }
-
-        let coef = {
-            mulher: {
-                negro: { C_Age: 17.114, C_HDL_Chol: -18.92, C_Age_HDL_Chol: 4.475, C_On_Hypertension_Meds: 29.291, C_Age_On_Hypertension_Meds: -6.432, C_Off_Hypertension_Meds: 27.82, C_Age_Off_Hypertension_Meds: -6.087, C_Smoker: 0.691, C_Diabetes: 0.874, S10: 0.9533, Mean_Terms: 86.61 },
-                branco: { C_Age: -29.799, C_HDL_Chol: -13.578, C_Age_HDL_Chol: 3.149, C_On_Hypertension_Meds: 2.019, C_Off_Hypertension_Meds: 1.957, C_Smoker: 7.574, C_Age_Smoker: -1.665, C_Diabetes: 0.661, S10: 0.9665, Mean_Terms: -29.18 }
-            },
-            homem: {
-                negro: { C_Age: 2.469, C_HDL_Chol: -0.307, C_On_Hypertension_Meds: 1.916, C_Off_Hypertension_Meds: 1.809, C_Smoker: 0.549, C_Diabetes: 0.645, S10: 0.8954, Mean_Terms: 19.54 },
-                branco: { C_Age: 12.344, C_HDL_Chol: -7.99, C_Age_HDL_Chol: 1.769, C_On_Hypertension_Meds: 1.797, C_Off_Hypertension_Meds: 1.764, C_Smoker: 7.837, C_Age_Smoker: -1.795, C_Diabetes: 0.658, S10: 0.9144, Mean_Terms: 61.18 }
+    
+        let C_Age,
+            C_Sq_Age,
+            C_Total_Chol,
+            C_Age_Total_Chol,
+            C_HDL_Chol,
+            C_Age_HDL_Chol,
+            C_On_Hypertension_Meds,
+            C_Age_On_Hypertension_Meds,
+            C_Off_Hypertension_Meds,
+            C_Age_Off_Hypertension_Meds,
+            C_Smoker,
+            C_Age_Smoker,
+            C_Diabetes,
+            S10,
+            Mean_Terms;
+    
+        if (genero === "mulher") {
+            if (raca === "negro") {
+                C_Age = 17.114;
+                C_Sq_Age = 0;
+                C_Total_Chol = 0.94;
+                C_Age_Total_Chol = 0;
+                C_HDL_Chol = -18.92;
+                C_Age_HDL_Chol = 4.475;
+                C_On_Hypertension_Meds = 29.291;
+                C_Age_On_Hypertension_Meds = -6.432;
+                C_Off_Hypertension_Meds = 27.82;
+                C_Age_Off_Hypertension_Meds = -6.087;
+                C_Smoker = 0.691;
+                C_Age_Smoker = 0;
+                C_Diabetes = 0.874;
+                S10 = 0.9533;
+                Mean_Terms = 86.61;
+            } else if (raca === "branco") {
+                C_Age = -29.799;
+                C_Sq_Age = 4.884;
+                C_Total_Chol = 13.54;
+                C_Age_Total_Chol = -3.114;
+                C_HDL_Chol = -13.578;
+                C_Age_HDL_Chol = 3.149;
+                C_On_Hypertension_Meds = 2.019;
+                C_Age_On_Hypertension_Meds = 0;
+                C_Off_Hypertension_Meds = 1.957;
+                C_Age_Off_Hypertension_Meds = 0;
+                C_Smoker = 7.574;
+                C_Age_Smoker = -1.665;
+                C_Diabetes = 0.661;
+                S10 = 0.9665;
+                Mean_Terms = -29.18;
             }
-        };
-
-        let c = coef[genero][raca];
-
-        let Terms = c.C_Age * Math.log(idade) +
-            c.C_Sq_Age * Math.pow(Math.log(idade), 2) +
-            c.C_Total_Chol * Math.log(colesterolTotal) +
-            c.C_Age_Total_Chol * Math.log(idade) * Math.log(colesterolTotal) +
-            c.C_HDL_Chol * Math.log(colesterolHDL) +
-            c.C_Age_HDL_Chol * Math.log(idade) * Math.log(colesterolHDL) +
-            (emMedicamentoHipertensao ? c.C_On_Hypertension_Meds * Math.log(pressaoSistolica) +
-            c.C_Age_On_Hypertension_Meds * Math.log(idade) * Math.log(pressaoSistolica) :
-            c.C_Off_Hypertension_Meds * Math.log(pressaoSistolica) +
-            c.C_Age_Off_Hypertension_Meds * Math.log(idade) * Math.log(pressaoSistolica)) +
-            c.C_Smoker * fumante +
-            (c.C_Age_Smoker || 0) * Math.log(idade) * fumante +
-            c.C_Diabetes * diabetes;
-
-
-        let Risco_Dez_Anos = 100 * (1 - Math.pow(c.S10, Math.exp(Terms - c.Mean_Terms)));
-
-        let resultado = Risco_Dez_Anos < 5 ? "Baixo risco (<5%)" :
-            Risco_Dez_Anos <= 7.4 ? "Limítrofe (5% a 7.4%)" :
-                Risco_Dez_Anos <= 19.9 ? "Risco intermediário (7.5% a 19.9%)" :
-                    "Alto risco (≥20%)";
-
-                    console.log(Risco_Dez_Anos);
-        let resultadoTexto = `Risco de Dez Anos: ${Risco_Dez_Anos.toFixed(2)}% - ${resultado}`;
-        $('#resultadoRisco').text(resultadoTexto).data('copyText', resultadoTexto).attr('data-copy', true);
+        } else if (genero === "homem") {
+            if (raca === "negro") {
+                C_Age = 2.469;
+                C_Sq_Age = 0;
+                C_Total_Chol = 0.302;
+                C_Age_Total_Chol = 0;
+                C_HDL_Chol = -0.307;
+                C_Age_HDL_Chol = 0;
+                C_On_Hypertension_Meds = 1.916;
+                C_Age_On_Hypertension_Meds = 0;
+                C_Off_Hypertension_Meds = 1.809;
+                C_Age_Off_Hypertension_Meds = 0;
+                C_Smoker = 0.549;
+                C_Age_Smoker = 0;
+                C_Diabetes = 0.645;
+                S10 = 0.8954;
+                Mean_Terms = 19.54;
+            } else if (raca === "branco") {
+                C_Age = 12.344;
+                C_Sq_Age = 0;
+                C_Total_Chol = 11.853;
+                C_Age_Total_Chol = -2.664;
+                C_HDL_Chol = -7.99;
+                C_Age_HDL_Chol = 1.769;
+                C_On_Hypertension_Meds = 1.797;
+                C_Age_On_Hypertension_Meds = 0;
+                C_Off_Hypertension_Meds = 1.764;
+                C_Age_Off_Hypertension_Meds = 0;
+                C_Smoker = 7.837;
+                C_Age_Smoker = -1.795;
+                C_Diabetes = 0.658;
+                S10 = 0.9144;
+                Mean_Terms = 61.18;
+            }
+        }
+    
+        let Terms =
+            C_Age * Math.log(idade) +
+            C_Sq_Age * Math.pow(Math.log(idade), 2) +
+            C_Total_Chol * Math.log(colesterolTotal) +
+            C_Age_Total_Chol * Math.log(idade) * Math.log(colesterolTotal) +
+            C_HDL_Chol * Math.log(colesterolHDL) +
+            C_Age_HDL_Chol * Math.log(idade) * Math.log(colesterolHDL) +
+            emMedicamentoHipertensao *
+                C_On_Hypertension_Meds *
+                Math.log(pressaoSistolica) +
+            emMedicamentoHipertensao *
+                C_Age_On_Hypertension_Meds *
+                Math.log(idade) *
+                Math.log(pressaoSistolica) +
+            !emMedicamentoHipertensao *
+                C_Off_Hypertension_Meds *
+                Math.log(pressaoSistolica) +
+            !emMedicamentoHipertensao *
+                C_Age_Off_Hypertension_Meds *
+                Math.log(idade) *
+                Math.log(pressaoSistolica) +
+            C_Smoker * fumante +
+            C_Age_Smoker * Math.log(idade) * fumante +
+            C_Diabetes * diabetes;
+    
+        let Risco_Dez_Anos =
+            100 * (1 - Math.pow(S10, Math.exp(Terms - Mean_Terms)));
+    
+        let resultado = "";
+        if (Risco_Dez_Anos < 5) {
+            resultado = "Baixo risco (<5%)";
+        } else if (Risco_Dez_Anos >= 5 && Risco_Dez_Anos <= 7.4) {
+            resultado = "Limítrofe (5% a 7.4%)";
+        } else if (Risco_Dez_Anos >= 7.5 && Risco_Dez_Anos <= 19.9) {
+            resultado = "Risco intermediário (7.5% a 19.9%)";
+        } else {
+            resultado = "Alto risco (≥20%)";
+        }
+    
+        $('#resultadoRisco').text(
+            "Risco de Dez Anos: " + Risco_Dez_Anos.toFixed(2) + "% - " + resultado
+        );
     });
+    
 
     // Calculadora de Colesterol LDL
     $('#calcularLDLBtn').click(function () {
