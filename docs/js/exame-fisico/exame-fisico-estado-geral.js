d3349f0ae988a5b9e0fe9d761cc47383b4f3678e
Rodrigo Voigt Filho <rodrigo.voigt.f@gmail.com>
2025-02-09 14:13:04 -0300

    form

--- docs/js/exame-fisico/exame-fisico-estado-geral.js
+++ docs/js/exame-fisico/exame-fisico-estado-geral.js
@@ -24,19 +24,19 @@ $(document).ready(function () {
             textoGerado.push("O: Bom estado geral, fácies atípica, boas condições de higiene, colaborativo, eutímico, normobúlico, memória e raciocínio preservados, lúcido, orientado autopsiquicamente e alopsiquicamente, normovigilante. Eutrófico (IMC calculadora), normotônico, normoativo, deambulando sem auxílio. Mucosas úmidas e normocoradas, anictérico, acianótico, afebril (não aferido). Normosfigmo, normotenso, eupneico, normoxia em ar ambiente.\n");
         }
     
-        if ($('#examePele').is(':checked')) {
+        if ($('#exameFisicoPele').is(':checked')) {
             textoGerado.push("Exame físico de pele:\nLesão do tipo () localizada em (), simétrica, bordas definidas, cor (hiper/normo/hipocrômica/hiperemiada), diâmetro de (), de surgimento há () com/sem alterações significativas recentemente.\n");
         }
     
-        if ($('#exameTorax').is(':checked')) {
+        if ($('#exameFisicoTorax').is(':checked')) {
             textoGerado.push("Exame físico de tórax e pulmonar:\nTórax íntegro, sem massas, crostas ou descamação, simétrico, sem abaulamento, sem retração, formato elíptico, normolíneo (ângulo de charpy =90°)/longilíneo (ângulo de charpy <90°)/brevilíneo (ângulo de charpy >90°).\nSom claro pulmonar à percussão.\nExpansão simétrica bilateralmente.\nFrêmito toracovocal presente sem alterações.\nMurmúrios vesiculares presentes bilateralmente, simétricos, sem ruídos adventícios.\n");
         }
     
-        if ($('#exameCardio').is(':checked')) {
+        if ($('#exameFisicoCardio').is(':checked')) {
             textoGerado.push("Exame físico cardiovascular:\nPalpação do pulso de frequência normal, amplitude mediana ++/3, ritmo regular, simétrico com lado contralateral.\nAusência de turgência jugular.\nIctus cordis palpável na linha hemoclavicular esquerda no ()° espaço intercostal.\nAusência de impulsão paraesternal esquerda.\nAusência de frêmito cardiovascular.\nBulhas rítmicas regulares normofonéticas em dois tempos.\n");
         }
     
-        if ($('#exameAbdomen').is(':checked')) {
+        if ($('#exameFisicoAbdomen').is(':checked')) {
             textoGerado.push("Exame físico de abdômen:\nAbdômen plano, depressível, com ruídos hidroaéreos presentes normoativos.\nSem alterações de sensibilidade, sem alterações de continuidade ou distensão após manobra de valsalva, normotenso bilateralmente.\nSom predominantemente claro e timpânico. Sem dor à palpação profunda ou superficial. Sem visceromegalias.\nSinal de piparote negativo.\nMacicez móvel normal.\nFígado palpável ao método de Lemos-Torres com bordas lisas, regulares, delimitadas e aspecto macio.\n");
         }
     
